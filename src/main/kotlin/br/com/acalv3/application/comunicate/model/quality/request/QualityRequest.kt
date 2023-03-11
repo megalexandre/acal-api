abdc5068aa249bacccdc3c542e196b89beb794af
Alexandre Queiroz <megalexandre@gmail.com>
2023-03-11 09:49:43 -0300

    Refatoração

--- src/main/kotlin/br/com/acalv3/application/comunicate/model/quality/request/QualityRequest.kt
+++ src/main/kotlin/br/com/acalv3/application/comunicate/model/quality/request/QualityRequest.kt
@@ -8,39 +8,50 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING
 import java.time.LocalDate
 import java.util.UUID
+import javax.validation.constraints.NotNull
+import org.springframework.validation.annotation.Validated
 
+@Validated
 class QualityRequest(
 
-    val gathering: List<GatheringRequest>,
+    @field:NotNull(message = "os parametros da coleta são obrigatório")
+    val gathering: List<GatheringRequest>? ,
 
+    @field:NotNull(message = "Data de inicio é obrigatório")
     @JsonFormat(shape = STRING, pattern = DATE_FORMAT)
-    val startedAt: LocalDate,
+    val startedAt: LocalDate?,
 
+    @field:NotNull(message = "Data final é obrigatório")
     @JsonFormat(shape = STRING, pattern = DATE_FORMAT)
-    val finishedAt: LocalDate,
+    val finishedAt: LocalDate?,
 )
 
 fun QualityRequest.toQuality() = Quality(
     id = UUID.randomUUID().toString(),
-    startedAt = startedAt,
-    finishedAt = finishedAt,
+    startedAt = startedAt ?: throw RuntimeException(),
+    finishedAt = finishedAt ?: throw RuntimeException(),
 ).also {
-    it.gathering = gathering.toGathering(it)
+    it.gathering = gathering?.toGathering(it) ?: throw RuntimeException()
 }
 
+@Validated
 class GatheringRequest(
-    val param: String,
-    val required: Long,
-    val analyzed: Long,
-    val conformity: Long,
+    @field:NotNull(message = "Um parametro deve ter um nome")
+    val param: String?,
+    @field:NotNull(message = "Quantidade exigida é obrigatorio")
+    val required: Long?,
+    @field:NotNull(message = "Quantidade analisado é obrigatorio")
+    val analyzed: Long?,
+    @field:NotNull(message = "Quantidade Confirmidade é obrigatorio")
+    val conformity: Long?,
 )
 
 fun GatheringRequest.toGathering(quality: Quality) = Gathering(
     id = UUID.randomUUID().toString(),
-    required = required,
+    required = required ?: throw RuntimeException(),
+    analyzed = analyzed ?: throw RuntimeException(),
+    conformity = conformity ?: throw RuntimeException(),
     param = Param.byValue(param) ?: throw RuntimeException("Param $param is not found"),
-    analyzed = analyzed,
-    conformity = conformity,
     quality = quality
 )
 
---
+++ src/test/kotlin/br/com/acalv3/integration/quality/validation/QualityValidationTest.kt
@@ -0,0 +1,52 @@
+package br.com.acalv3.integration.quality.validation
+
+import br.com.acalv3.integration.DefaultGatewayTest
+import br.com.acalv3.resources.model.business.CustomerEntity
+import io.restassured.http.ContentType.JSON
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import java.nio.charset.Charset.defaultCharset
+import java.util.UUID
+import org.hamcrest.Matchers.hasKey
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.util.StreamUtils.copyToString
+
+class QualityValidationTest: DefaultGatewayTest() {
+
+	@Value("classpath:json/request/quality/validation/quality-without-param.json")
+	private lateinit var request: Resource
+
+	@Autowired
+	private lateinit var repository: JpaRepository<CustomerEntity, UUID>
+
+	@BeforeEach
+	fun beforeEach(){
+		repository.deleteAll()
+	}
+
+	@Test
+	fun `should save a minimal quality ok 200`(){
+		val request = copyToString(request.inputStream, defaultCharset())
+
+		Given {
+			contentType(JSON)
+			header(header)
+			body(request)
+		} When {
+			post("$basePath/quality")
+		} Then {
+			statusCode(BAD_REQUEST.value())
+			body("$", hasKey("id"))
+		}
+	}
+
+}
