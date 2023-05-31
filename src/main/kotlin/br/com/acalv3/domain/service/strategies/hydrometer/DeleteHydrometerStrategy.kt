658e8afef4bbf8f377a8e344848dc74655746ad8
Alexandre Queiroz <megalexandre@gmail.com>
2023-05-31 16:39:00 -0300

    zero sonar warning

--- src/main/kotlin/br/com/acalv3/domain/service/strategies/hydrometer/DeleteHydrometerStrategy.kt
+++ src/main/kotlin/br/com/acalv3/domain/service/strategies/hydrometer/DeleteHydrometerStrategy.kt
@@ -2,15 +2,18 @@ package br.com.acalv3.domain.service.strategies.hydrometer
 
 import br.com.acalv3.domain.enumeration.Action.DELETE
 import br.com.acalv3.domain.model.Hydrometer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class DeleteHydrometerStrategy: HydrometerStrategy<Hydrometer> {
+    private var logger: Logger = LoggerFactory.getLogger(this::class.java)
 
     override fun action() = DELETE
 
     override fun can(model: Hydrometer) {
-
+        logger.info("delete hydrometer")
     }
 
 }
--- src/main/kotlin/br/com/acalv3/domain/service/strategies/quality/DeleteQualityStrategy.kt
+++ src/main/kotlin/br/com/acalv3/domain/service/strategies/quality/DeleteQualityStrategy.kt
@@ -2,13 +2,18 @@ package br.com.acalv3.domain.service.strategies.quality
 
 import br.com.acalv3.domain.enumeration.Action.DELETE
 import br.com.acalv3.domain.model.Quality
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class QualityQualityStrategy: QualityStrategy<Quality> {
+class DeleteQualityStrategy: QualityStrategy<Quality> {
+    private var logger: Logger = LoggerFactory.getLogger(this::class.java)
 
     override fun action() = DELETE
 
-    override fun can(model: Quality) {}
+    override fun can(model: Quality) {
+        logger.info("delete quality")
+    }
 
 }
